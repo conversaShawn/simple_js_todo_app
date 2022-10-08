c7acd18e5dceedb2fe9f5bc9e49bcc53a237c42b
Shawn Harris <shawn.harris3510@gmail.com>
2022-10-07 21:31:18 -0700

    trashcan image

--- app.js
+++ app.js
@@ -1,18 +1,5 @@
 console.log("js file works");
 
-// const toggleComplete = () => {
-//   // get all todos
-//   let allToDos = document.querySelectorAll(".individualToDo");
-//   // loop through todos and addEventListener
-//   console.log(allToDos);
-//   allToDos.forEach((singleToDo) => {
-//     singleToDo.addEventListener("click", function () {
-//       singleToDo.classList.toggle("strikeout");
-//     });
-//   });
-// };
-// toggleComplete();
-
 // mark complete
 const toggleComplete = () => {
   // console.log("it toggles");
@@ -26,9 +13,26 @@ const toggleComplete = () => {
 };
 toggleComplete();
 
+delete complete;
+const addTrashCan = () => {
+  // console.log("it deletes");
+  // get parent node
+  let deleteToDo = document.querySelectorAll("li");
+
+  // add event listener
+
+  deleteToDo.forEach((st) => {
+    let trashcan = document.createElement("div");
+    trashcan.classList.add("testing");
+    st.appendChild(trashcan);
+    
+  });
+};
+addTrashCan();
+
 // add todo
 const addNewToDo = () => {
-  console.log("add todo on click");
+  // console.log("add todo on click");
   // store input text as variable
   let newToDoText = document.getElementById("newToDo").value;
   // create element
@@ -36,17 +40,17 @@ const addNewToDo = () => {
   // create text node
   let textNode = document.createTextNode(newToDoText);
   // append text to element
+  let trashcan = document.createElement("div");
+  // trashcan.classList.add('testing')
   element.appendChild(textNode);
+  element.appendChild(trashcan).classList.add("testing");
   // append text node to todo list
+
   document
     .getElementById("toDoList")
     .appendChild(element)
     .classList.add("individualToDo");
   console.log(element);
-// all event listener to all new todos
-// element.addEventListener('click', () => {
-//   element.classList.toggle('strikeout')
-// })
 
   clearInput();
 };
@@ -56,7 +60,3 @@ const clearInput = () => {
   console.log("clear input");
   document.getElementById("newToDo").value = "";
 };
-
-// add eventlistener to all incomplete todos
-
-// delete todo
