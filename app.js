039143500bbde47189e09ec2ad1b51b75a0f6eae
Shawn Harris <shawn.harris3510@gmail.com>
2022-10-10 21:02:39 -0700

    edit and save button work

--- app.js
+++ app.js
@@ -1,5 +1,41 @@
 // console.log("js file works");
 
+// ADD EDIT BUTTON
+const addPencil = () => {
+  // console.log('content editable')
+
+  let addEditIcons = document.querySelectorAll("li");
+
+  addEditIcons.forEach((individualToDo) => {
+    let toDoInput = document.createElement("div");
+    toDoInput.classList.add("testing");
+    toDoInput.innerText = "Learn js"
+    individualToDo.appendChild(toDoInput)
+
+    let pencil = document.createElement("div");
+    pencil.classList.add("editToDo");
+    individualToDo.appendChild(pencil);
+    pencil.addEventListener("click", () => {
+      toDoInput.contentEditable = true;
+      toDoInput.focus();
+      pencil.style.display = "none";
+      saveButton.style.display = "block";
+      // console.log("pencil click");
+    });
+    let saveButton = document.createElement("div");
+    saveButton.classList.add("saveToDo");
+    individualToDo.appendChild(saveButton);
+    saveButton.style.display = "none";
+    saveButton.addEventListener("click", () => {
+      toDoInput.contentEditable = false;
+      saveButton.style.display = "none";
+      pencil.style.display = "block";
+      // console.log('save clicked')
+    });
+  });
+};
+addPencil();
+
 // ADD DELETE BUTTON;
 const addTrashCan = () => {
   // console.log("it deletes");
@@ -49,12 +85,48 @@ const addNewToDo = () => {
   // CREATE NEW TODO DIV
   // store input text as variable
   let newToDoText = document.getElementById("newToDo").value;
-  // create element
+  // create parent element
   let element = document.createElement("li");
+  // create element
+  let toDoInput = document.createElement("div");
+  toDoInput.classList.add("testing");
   // create text node
   let textNode = document.createTextNode(newToDoText);
-  // append text to element
-  element.appendChild(textNode);
+  // append text to input
+  toDoInput.appendChild(textNode);
+  // append input to element
+  element.appendChild(toDoInput);
+
+  // ADD EDIT BUTTON TO NEW TODO
+  // create edit element
+  let pencil = document.createElement("div");
+  pencil.classList.add("editToDo");
+  // append edit button to element
+  element.appendChild(pencil);
+
+  // create save element
+  let saveButton = document.createElement("div");
+  saveButton.classList.add("saveToDo");
+  saveButton.style.display = "none";
+  // append save button to element
+  element.appendChild(saveButton);
+
+  // add event listener to delete button
+  pencil.addEventListener("click", () => {
+    toDoInput.contentEditable = true;
+    toDoInput.focus();
+    pencil.style.display = "none";
+    saveButton.style.display = "block";
+    // console.log("pencil click");
+  });
+
+  // add event listener to delete button
+  saveButton.addEventListener("click", () => {
+    toDoInput.contentEditable = false;
+    saveButton.style.display = "none";
+    pencil.style.display = "block";
+    // console.log("save click");
+  });
 
   // ADD DELETE BUTTON TO NEW TODO
   // create element
