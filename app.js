edc840f2d53ce5836abac2b78e343ee5bb92f6a6
Shawn Harris <shawn.harris3510@gmail.com>
2022-10-18 18:34:19 -0700

    checkbox

--- app.js
+++ app.js
@@ -60,20 +60,29 @@ addTrashCan();
 const addCheckBox = () => {
   // console.log("it toggles");
 
+// 
+
+
   // get parent node
   let addCheckBoxIcon = document.querySelectorAll("li");
 
   // TOGGLE COMPLETE/INCOMPLETE
   addCheckBoxIcon.forEach((singleCheckBox) => {
+// create checkBox before div
+let controlCB = document.createElement('input')
+controlCB.type = "checkbox"
+controlCB.classList.add('reallyChecked')
+
     let checkbox = document.createElement("div");
     checkbox.classList.add("checkToDo");
     singleCheckBox.prepend(checkbox);
-    checkbox.addEventListener("click", () => {
-      // toggle class
-      checkbox.parentElement.classList.toggle("markComplete");
-      checkbox.classList.toggle("markCompleteCheckBox");
-      // console.log("clicks");
-    });
+    singleCheckBox.prepend(controlCB)
+    // checkbox.addEventListener("click", () => {
+    //   // toggle class
+    //   checkbox.parentElement.classList.toggle("markComplete");
+    //   checkbox.classList.toggle("markCompleteCheckBox");
+    //   // console.log("clicks");
+    // });
   });
 };
 addCheckBox();
