fa5f95e4f93519864b145405139302f25a61371a
Shawn Harris <shawn.harris3510@gmail.com>
2022-11-01 22:01:17 -0700

    individual delete button works

--- js/add.js
+++ js/add.js
@@ -20,50 +20,12 @@ const addNewToDo = () => {
     let toDoList = document.getElementById('toDoList')
     toDoList.innerHTML += itemTemplate.innerHTML.replace('ITEM_TEXT', newToDoText)
 
-
     addTrashCan()
     
-    // // CREATE NEW TODO DIV
-    // // store input text as variable
-    // // create parent element
-    // let element = document.createElement("li");
-    // // create element
-    // let toDoInput = document.createElement("div");
-    // toDoInput.classList.add("testing");
-    // // create text node
-    // let textNode = document.createTextNode(newToDoText);
-    // // append text to input
-    // toDoInput.appendChild(textNode);
-    // // append input to element
-    // element.appendChild(toDoInput);
-
-    // // create element
-    // let checkbox = document.createElement("div");
-    // checkbox.classList.add("checkToDo");
-    // // prepend complete/incomplete button to element
-    // element.prepend(checkbox);
-    // element.prepend(controlCB);
-    // // add event listener to complete/incomplete button
-    // controlCB.addEventListener("click", (todo) => {
-    //   // toggle class
-    //   checkbox.parentElement.classList.toggle("markComplete");
-    //   // checkbox.classList.toggle("markCompleteCheckBox");
-    //   // console.log('toggles complete')
-    // });
-
-    // newToDoText === ""
-    // ? alert("YOU MUST ENTER NEW TODO")
-    // : document
-    // .getElementById("toDoList")
-    // .appendChild(element)
-    // .classList.add("individualToDo");
-    // console.log(element);
-    // APPEND TEXT NODE TO TODO LIST OR THROW ERROR IF TEXT INPUT EMPTY
     clearInput();
   });
 
 };
-// addNewToDo();
 
 // RESET INPUT
 const clearInput = () => {
--- js/delete.js
+++ js/delete.js
@@ -1,15 +1,20 @@
 // ADD DELETE BUTTON;
 const addTrashCan = () => {
-
   let trashcans = document.getElementsByClassName("deleteToDo");
 
   for (let i = 0; i < trashcans.length; i++) {
     trashcans[i].addEventListener("click", () => {
-      console.log("delete!");
+        if (trashcans.length > 1) {
+
             const individualTrashCan = trashcans[i];
-            individualTrashCan.parentNode.remove()
-    //   trashcans[i].parentElement.remove();
-    console.log("how many left: ", trashcans.length);
+            individualTrashCan.parentNode.remove(individualTrashCan);
+            //   trashcans[i].parentElement.remove();
+            console.log("how many left: ", trashcans.length);
+        }
+        else {
+            trashcans[0].parentNode.remove()
+            console.log("how many left: ", trashcans.length);
+        }
     });
   }
 };
