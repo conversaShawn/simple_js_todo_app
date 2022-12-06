e396f45d91710be2b764be9fd520b3e3b4e35e99
Shawn Harris <shawn.harris3510@gmail.com>
2022-12-05 21:02:46 -0500

    remove outdated code

--- js/complete.js
+++ js/complete.js
@@ -1,27 +1,23 @@
-// ADD COMPLETE/INCOMPLETE BUTTON;
 const completeToDo = () => {
-  // console.log("it toggles");
   let addCheckBoxIcon = document.querySelectorAll(".reallyChecked");
 
   for (let i = 0; i < addCheckBoxIcon.length; i++) {
     let eventParent = addCheckBoxIcon[i].parentNode;
     addCheckBoxIcon[i].addEventListener("click", () => {
-      if (eventParent.querySelector('.checkToDo').classList.contains("checkComplete")) {
-        // console.log(addCheckBoxIcon[i]);
-        // console.log(addCheckBoxIcon[i].checked);
-        // console.log(eventParent);
-        console.log(eventParent.querySelector('.reallyChecked').checked)
-        eventParent.querySelector('.checkToDo').classList.remove("checkComplete")
-        eventParent.querySelector('.toDoText').classList.remove("markComplete")
+      if (
+        eventParent
+          .querySelector(".checkToDo")
+          .classList.contains("checkComplete")
+      ) {
+        console.log(eventParent.querySelector(".reallyChecked").checked);
+        eventParent
+          .querySelector(".checkToDo")
+          .classList.remove("checkComplete");
+        eventParent.querySelector(".toDoText").classList.remove("markComplete");
       } else {
-      // if (reallyCheckedNode.checked) {
-        // console.log(addCheckBoxIcon[i]);
-        // eventParent.querySelector('.reallyChecked').checked = false
-        // console.log(addCheckBoxIcon[i].checked);
-        // console.log(eventParent);
-        console.log(eventParent.querySelector('.reallyChecked').checked)
-        eventParent.querySelector('.checkToDo').classList.add("checkComplete")
-        eventParent.querySelector('.toDoText').classList.add("markComplete")
+        console.log(eventParent.querySelector(".reallyChecked").checked);
+        eventParent.querySelector(".checkToDo").classList.add("checkComplete");
+        eventParent.querySelector(".toDoText").classList.add("markComplete");
       }
     });
   }
