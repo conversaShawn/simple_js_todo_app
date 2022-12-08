8f60a637cf2e50fa4c32a38f2c75b389c0153fca
Shawn Harris <shawn.harris3510@gmail.com>
2022-12-07 22:51:48 -0500

    add new todo pseudocode

--- cypress/e2e/add.cy.js
+++ cypress/e2e/add.cy.js
@@ -1,6 +1,18 @@
 describe('add a new todo', () => {
-  it('adds', () => {
+  it('adds with click', () => {
     // visit baseUrl
-    // get 
+    // verifies length of list (3)
+    // get newToDo input 
+    // type "Start learn.cypress.io"
+    // click addToDoButton button to add new todo
+    // verify length of list(4)
+  })
+  it('adds with enter', () => {
+    // visit baseUrl
+    // verify length of list (4)
+    // get newToDo input
+    // type "Live my best life"
+    // press "Enter" to add new todo
+    // verify length of list (5)
   })
 })
